6de69e4ef367248dbcb196b5c8e8c5419bb359c6
mkbn-consultancy <79524974+mkbn-consultancy@users.noreply.github.com>
2021-11-23 08:11:15 +0200

    adding new files

---
+++ Tigbur/FunctionsRectangleMenu/functionsDeclarations.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void getPositiveNumberFromUser(int& num);
+int handleMenuOptions(int width, int height);
---
+++ Tigbur/FunctionsRectangleMenu/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "functionsDeclarations.h"
+
+
+int main() { 
+	int width, height;
+
+	std::cout << "please enter width: ";
+	getPositiveNumberFromUser(width);	//jump ?? [address of getPositiveNumberFromUser]
+
+	std::cout << "please enter height: ";
+	getPositiveNumberFromUser(height);
+
+	int option;
+	do
+	{
+		option = handleMenuOptions(width,height);
+	} while (option != 4);
+
+	return 0;
+}
